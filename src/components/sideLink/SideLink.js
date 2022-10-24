4176b491b606c48d938bd0d7cc7736559da1e718
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-10-24 23:30:28 +0530

    sidelink added

---
+++ src/components/sideLink/SideLink.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SideLink = () => {
+  return <div>SideLink</div>;
+};
+
+export default SideLink;
--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import CenterBlock from '../../components/centerBlock/CenterBlock';
+import SideLink from '../../components/sideLink/SideLink';
 
 const MainPage = () => {
   return (
     <>
-      <CenterBlock />
+      <div>
+        <div>
+          <SideLink />
+        </div>
+        <div>
+          <CenterBlock />
+        </div>
+      </div>
     </>
   );
 };
